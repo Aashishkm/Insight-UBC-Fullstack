f432a93bb294930085caf1d7fc1b24d0607ff7ca
amehra123 <60304331+amehra123@users.noreply.github.com>
2023-03-12 12:22:24 -0700

    fixed up models

--- src/Models/RoomDatasetModel.ts
+++ src/Models/RoomDatasetModel.ts
@@ -3,10 +3,13 @@ import {CourseModel} from "./CourseModel";
 import {SectionModel} from "./SectionModel";
 import {DatasetModel} from "./DatasetModel";
 import {RoomModel} from "./RoomModel";
+import {BuildingModel} from "./BuildingModel";
 export class RoomDatasetModel extends DatasetModel {
 	public rooms: RoomModel[];
+	public buildings: BuildingModel[];
 	constructor(insightDataset: InsightDataset) {
 		super(insightDataset);
 		this.rooms = [];
+		this.buildings = [];
 	}
 }
