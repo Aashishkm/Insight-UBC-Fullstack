07775e05d1657e60c4112b78b7357639e52836c1
amehra123 <60304331+amehra123@users.noreply.github.com>
2023-03-14 19:07:09 -0700

    progress on parsing the html and adding rooms dataset

--- src/controller/RoomsProcessorHelper.ts
+++ src/controller/RoomsProcessorHelper.ts
@@ -6,7 +6,7 @@ import {RoomDatasetModel} from "../Models/RoomDatasetModel";
 export class RoomsProcessorHelper {
 	public parseRooms(indexData: string, processor: DataProcessorModel): Promise<any> {
 		let parsedIndex = parse(indexData);
-
+		let node: any;
 		return Promise.reject(new InsightError("bad"));
 	}
 
