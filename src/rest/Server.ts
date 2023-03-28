b647604493075addfdc2c98e09d8d0b4c599bf5a
Jack <jackt32@students.cs.ubc.ca>
2023-03-28 15:18:03 -0700

    Add DELETE and POST endpoints

--- src/rest/Server.ts
+++ src/rest/Server.ts
@@ -1,6 +1,7 @@
 import express, {Application, Request, Response} from "express";
 import * as http from "http";
 import cors from "cors";
+import ServerMethods from "../controller/ServerMethods";
 
 export default class Server {
 	private readonly port: number;
@@ -83,32 +84,19 @@ export default class Server {
 	private registerRoutes() {
 		// This is an example endpoint this you can invoke by accessing this URL in your browser:
 		// http://localhost:4321/echo/hello
-		this.express.get("/echo/:msg", Server.echo);
+		this.express.get("/echo/:msg", ServerMethods.echo);
 
 		// TODO: your other endpoints should go here
+		this.express.post("/query", ServerMethods.query);
 
+		this.express.delete("/dataset/:id", ServerMethods.remove);
 	}
+
 
 	/**
 	 * The next two methods handle the echo service.
 	 * These are almost certainly not the best place to put these, but are here for your reference.
 	 * By updating the Server.echo function pointer above, these methods can be easily moved.
 	 */
-	private static echo(req: Request, res: Response) {
-		try {
-			console.log(`Server::echo(..) - params: ${JSON.stringify(req.params)}`);
-			const response = Server.performEcho(req.params.msg);
-			res.status(200).json({result: response});
-		} catch (err) {
-			res.status(400).json({error: err});
-		}
-	}
 
-	private static performEcho(msg: string): string {
-		if (typeof msg !== "undefined" && msg !== null) {
-			return `${msg}...${msg}`;
-		} else {
-			return "Message not provided";
-		}
-	}
 }
